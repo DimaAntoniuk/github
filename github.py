18767eccce46c711a28634312755903abe2d3fa0
DimaAntnoiuk <dimaantoniuk09@gmail.com>
2021-10-20 23:05:39 +0300

    feat: add check

--- github.py
+++ github.py
@@ -3,14 +3,30 @@ from  selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 URL = 'http://google.com'
+STRING = 'github'
+
+def setup():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    return driver
+
+def go_to_url(driver, url):
+    driver.get(url)
 
 def find_page(driver, serach_string):
     searcher = driver.find_element_by_css_selector("input[type='text']")
     searcher.send_keys(f'{serach_string}')
     searcher.submit()
 
-with webdriver.Chrome(ChromeDriverManager().install()) as driver:
-    driver.get(URL)
-    time.sleep(1)
-    find_page(driver, 'github')
+def check_result(driver, search_string):
+    result = f'/search?q={search_string}' in driver.current_url
+    return result
+
+if __name__ == "__main__":
+    my_driver = setup()
+    go_to_url(my_driver, URL)
+    find_page(my_driver, STRING)
+    if check_result(my_driver, STRING):
+        print('HOORAY!')
+    else:
+        print('WRONG URL(((')
     time.sleep(3)
